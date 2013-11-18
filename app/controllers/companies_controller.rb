60b24642c8a8d84beb7ae7daa8c740ac4d019116
Brett Dudo <brett.dudo@gmail.com>
2013-11-18 12:03:50 -0800

    snippit CORS

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -30,6 +30,10 @@ class CompaniesController < ApplicationController
   end
 
   def snippet
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     @company = Company.find(params[:id])
     @trees = @company.trees
     @water = @company.water
