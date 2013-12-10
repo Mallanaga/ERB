d59aef17dcbadb172bcf53580c7c423e083726cc
Brett Dudo <brett.dudo@gmail.com>
2013-12-10 15:57:24 -0800

    quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,7 @@ class Quote < ActiveRecord::Base
   attr_accessible :name, :company, :email, :phone, :message,
                   :type_box, :length, :width, :height, :quantity
 
-  validates :name, :phone, :company
+  validates :name, :phone, :company,
             :length, :width, :height, :quantity, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
